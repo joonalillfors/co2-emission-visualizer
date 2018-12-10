520bc2bb608cc2c71cc5bfc368ba4f47d0aa26e8
Joona Lillfors <joona.lillfors@aalto.fi>
2018-12-10 16:06:47 +0200

    Heroku up

---
+++ client/build/precache-manifest.bf6a60498b33d10371e6a62ab19a1811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd1963b0146b47794e69",
+    "url": "/static/js/main.cd1963b0.chunk.js"
+  },
+  {
+    "revision": "77647a4d046add9b55ce",
+    "url": "/static/js/1.77647a4d.chunk.js"
+  },
+  {
+    "revision": "cd1963b0146b47794e69",
+    "url": "/static/css/main.5a53dad8.chunk.css"
+  },
+  {
+    "revision": "2f8be8b97b005d798351a8628a6d9682",
+    "url": "/index.html"
+  }
+];
